aa0b015ad7f05f64d80f986358d693d132111f7f
jstory1122 <jstory1122@gmail.com>
2022-10-29 11:04:38 +0900

    인덱스 국어 찾아서 합계 및 평균

--- TP2.py
+++ TP2.py
@@ -1,7 +1,8 @@
 def score(subject, student1, student2, student3,):
     subject,student1,student2,student3=['국어','수학','영어','과학','체육'],[],[],[]
-def main():
-        
+    index_num=subject.index('국어')
+    korean_sum=student1[index_num]+student2[index_num]+student3[index_num]
+    korean_sum2=student1[index_num]+student2[index_num]+student3[index_num]/3
 
 if __name__ == "__main__":
     main()
