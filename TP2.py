f707bbc37ee158e85cd37978241be3da1c51305f
whwhwh25 <jojungmin1289@gmail.com>
2022-10-29 10:43:04 +0900

    2

--- TP2.py
+++ TP2.py
@@ -1,6 +1,12 @@
 def score(subject, student1, student2, student3,):
     subject,student1,student2,student3=['국어','수학','영어','과학','체육'],[],[],[]
+    print(sum(student1)+sum(student2)+sum(student3)),'/n',(sum(student1)+sum(student2)+sum(student3)/15)
+
+
+
 def main():
+
+
         
 
 if __name__ == "__main__":
