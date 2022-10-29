83097dab3a51d1f3532d9b5939a3ec1db073e573
kmj06 <kjjuni2006@gmail.com>
2022-10-29 10:48:19 +0900

    각학생들_1

--- TP2.py
+++ TP2.py
@@ -1,7 +1,7 @@
 def score(subject, student1, student2, student3,):
     subject,student1,student2,student3=['국어','수학','영어','과학','체육'],[],[],[]
 def main():
-        
+    print(sum(student1),sum(student2),sum(student3)),'/n',((sum(student1),sum(student2),sum(student3))/5)
 
 if __name__ == "__main__":
     main()
